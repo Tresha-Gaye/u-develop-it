797ff18b0fca45336fb732cd30af8d32c7fb05db
Twigster <treshagaye@gmail.com>
2022-02-16 18:09:34 -0500

    added calls for inserting and deleting candidates

--- server.js
+++ server.js
@@ -19,8 +19,37 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// query to get object with all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {  
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+// Create candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            values (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 // Default response for any other request (NOT FOUND)- catchall route-make sure to put it last!
